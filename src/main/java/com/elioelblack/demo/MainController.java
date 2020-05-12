2669e10c9114503d42f57ec0357051a763f85d06
eliezer droid <eliohernandez94@gmail.com>
2020-05-12 15:57:39 -0600

    fix

--- src/main/java/com/elioelblack/demo/MainController.java
+++ src/main/java/com/elioelblack/demo/MainController.java
@@ -1,7 +1,19 @@
 package com.elioelblack.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MainController {
@@ -9,5 +21,7 @@ public class MainController {
 	public String index() {
 		return "Hola mundo";
 	}
+	
+	
 
 }
